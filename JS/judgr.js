3f47e36cb06508388d423e9b16c88994652787b2
DEREK HONG <hong.derek.j@gmail.com>
2015-09-05 12:20:55 -0400

    asdf

--- JS/judgr.js
+++ JS/judgr.js
@@ -6,27 +6,42 @@
 	Description: Sets up data for judging
 */
 
+var usedTags
+
 
 function requestUser() {
 
+	usedTags = [];
+
 	//obtain the Facebook ID of a random friend from 
-	//the list defined at the start of user login
-	var nextID = global_friendsList[Math.floor(Math.random()*global_friendsList.length)];
+	//the list defined at the start of user login AND REMOVE
+	//that friend so that you
+	var nextID = global_friendsList.splice(Math.floor(Math.random()*global_friendsList.length), 1)[0];
 
 	//set the profile picture to that user friend
 	FBgetProfilePicture(nextID, function(url) {
 		$("#ProfilePicture").attr("src", url);
-	})
+	});
 
-
-	//WORK IN PROGRESS===================================================
 	socket.emit('clientToServer', {
 		name: 'getProfile', 
 		hash: nextID
 	}, function(data) {
-		delete data["userId"];	//PURGE THE USER ID
+		delete data.userId;	//PURGE THE USER ID
 		var userTags = Object.keys(data);	//returns an array of the keys (sans userID)
+		console.log(userTags);
 
-		//console.log(userTags);
+		var tag = userTags.splice(Math.floor(Math.random()*userTags.length), 1)
+		$("#HashtagOne").text(tag[0]);
+		usedTags.push(tag[0]);
+
+		var tag = userTags.splice(Math.floor(Math.random()*userTags.length), 1)
+		$("#HashtagTwo").text(tag[0]);
+		usedTags.push(tag[0]);
+
+		var tag = userTags.splice(Math.floor(Math.random()*userTags.length), 1)
+		$("#HashtagThree").text(tag[0]);
+		usedTags.push(tag[0]);
 	});
 }
+

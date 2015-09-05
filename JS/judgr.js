32a8a9ed19d434d0fb9dfd9f8dc9834360375241
DEREK HONG <hong.derek.j@gmail.com>
2015-09-05 19:09:19 -0400

    ASDF

--- JS/judgr.js
+++ JS/judgr.js
@@ -24,71 +24,69 @@ function requestUser() {
 
 		global_nextID = global_friendsList.splice(Math.floor(Math.random()*global_friendsList.length), 1)[0];
 
-		if (!isValidUserID(global_nextID)) {
-			requestUser();
-		}
-		else {
-			global_usedTags = [];
+		FBgetProfilePicture(global_nextID, callback(url) {
+			$("#ProfilePicture").attr("src", url);
+		});
+		global_usedTags = [];
 
-			socket.emit('clientToServer', {
-				name: 'getProfile', 
-				hash: global_nextID
-			}, function(data) {
+		socket.emit('clientToServer', {
+			name: 'getProfile', 
+			hash: global_nextID
+		}, function(data) {
 
-				deferredArray = [];
+			deferredArray = [];
 
-				for(key in data) {
+			for(key in data) {
 
-					deferred = new $.Deferred();
-					deferredArray.push(deferred);
+				deferred = new $.Deferred();
+				deferredArray.push(deferred);
 
-					socket.emit('clientToServer', {
-						name: 'getHashtag', 
-						hash: key
-					}, function(data_2) {
+				socket.emit('clientToServer', {
+					name: 'getHashtag', 
+					hash: key
+				}, function(data_2) {
 
-						var temptags = Object.keys(data_2)
+					var temptags = Object.keys(data_2)
 
-						jQuery.extend(data, data_2);
+					jQuery.extend(data, data_2);
 
-						for(key in deferredArray) {
-							if(deferredArray[key].state() !== 'resolved') {
-								deferredArray[key].resolve();
-							}
+					for(key in deferredArray) {
+						if(deferredArray[key].state() !== 'resolved') {
+							deferredArray[key].resolve();
 						}
-					});
-				}
-
-				$.when.apply($, deferredArray).then(function() {
-					delete data.userId;
-					global_userTags = Object.keys(data);
-
-					if (global_userTags.length < 3) {
-						requestUser();
-					}
-					else {
-						var tag = global_userTags.splice(Math.floor(Math.random()*global_userTags.length), 1)
-						$("#HashtagOne").text(tag[0]);
-						global_usedTags.push(tag[0]);
-
-						var tag = global_userTags.splice(Math.floor(Math.random()*global_userTags.length), 1)
-						$("#HashtagTwo").text(tag[0]);
-						global_usedTags.push(tag[0]);
-
-						var tag = global_userTags.splice(Math.floor(Math.random()*global_userTags.length), 1)
-						$("#HashtagThree").text(tag[0]);
-						global_usedTags.push(tag[0]);
 					}
 				});
+			}
+
+			$.when.apply($, deferredArray).then(function() {
+				delete data.userId;
+				global_userTags = Object.keys(data);
+
+				if (global_userTags.length < 3) {
+					requestUser();
+				}
+				else {
+					var tag = global_userTags.splice(Math.floor(Math.random()*global_userTags.length), 1)
+					$("#HashtagOne").text(tag[0]);
+					global_usedTags.push(tag[0]);
+
+					var tag = global_userTags.splice(Math.floor(Math.random()*global_userTags.length), 1)
+					$("#HashtagTwo").text(tag[0]);
+					global_usedTags.push(tag[0]);
+
+					var tag = global_userTags.splice(Math.floor(Math.random()*global_userTags.length), 1)
+					$("#HashtagThree").text(tag[0]);
+					global_usedTags.push(tag[0]);
+				}
 			});
-		}
+		});
 	}
 }
 
 function updateProfile(hashname, value, callback) {
 	socket.emit('clientToServer', {
 		name: 'updateProfileScores',
-		hash: global_global_nextID,
+		hash: global_nextID,
 		attribute: hashname,
 		value: value
 	}, function(data, err) {

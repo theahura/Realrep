f642595c517f66c892117912d6a93933fc4b930b
DEREK HONG <hong.derek.j@gmail.com>
2015-09-05 08:52:37 -0400

    judgr modified

--- JS/judgr.js
+++ JS/judgr.js
@@ -6,14 +6,25 @@
 	Description: Sets up data for judging
 */
 
+$("#NewUserSelect").click(function() {
+	requestUser();
+});
+
 function requestUser() {
 
 	var nextID = global_friendsList[Math.floor(Math.random()*global_friendsList.length)];
+
+	FBgetProfilePicture(nextID, function(url) {
+		$("#ProfilePicture").attr("src", url);
+	})
 
 	socket.emit('clientToServer', {
 		name: 'getProfile', 
 		hash: nextID
 	}, function(data) {
-		console.log(data);
+		delete data["userId"];	//PURGE THE USER ID
+		var userTags = Object.keys(data);	//returns an array of the keys (sans userID)
+
+		console.log(userTags);
 	});
 }

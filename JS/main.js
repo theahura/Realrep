ad14e56448ebcff31db9fc0eb300263cf1123e30
DEREK HONG <hong.derek.j@gmail.com>
2015-09-05 04:24:40 -0400

    FBlogin and getName should be working now

--- JS/main.js
+++ JS/main.js
@@ -1,60 +1,6 @@
-var incomingObj = {
-	name: 'addUser',
-	hash: 'amolkapoor',
-	video_games: '10',
-	new_jersey: '10',
-	columbia: '10', 
-	nintendo: '10',
-	cats: '10'
-};
-
-socket.emit("clientToServer", incomingObj, function(data, err) {
-	console.log(data);
-	console.log(err);
-
-	incomingObj = {
-		name: 'getProfile', 
-		hash: 'amolkapoor'
-	}
-
-	socket.emit("clientToServer", incomingObj, function(data, err) {
-		console.log(data);
-		console.log(err);
-
-		incomingObj = {
-			name: 'getHashtag', 
-			hash: 'video_games'
-		}
-
-		socket.emit("clientToServer", incomingObj, function(data, err) {
-			console.log(data);
-			console.log(err);
-
-			incomingObj = {
-				name: 'updateProfileScores', 
-				hash: 'amolkapoor',
-				attribute: 'video_games', 
-				value: 1
-			}
-
-			socket.emit("clientToServer", incomingObj, function(data, err) {
-				console.log(data);
-				console.log(err);
-
-				incomingObj = {
-					name: 'updateProfileScores', 
-					hash: 'amolkapoor',
-					attribute: 'nintendo', 
-					value: -1
-				}
-
-				socket.emit("clientToServer", incomingObj, function(data, err) {
-					console.log(data);
-					console.log(err);
-				});
-			});
-		});
+//WHEN DAT BUTTON PRESS, LOG IN
+$("#FacebookLogin").click(function() {
+	FBlogin(function(id) {
+		global_ID = id;
 	});
 });
-
-

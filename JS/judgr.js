f18df59807f89eda9799f615ebc11172bf721bc0
Amol Kapoor <theahura@gmail.com>
2015-09-05 22:16:01 -0400

    Possible fix for judgr correlation data
    Signed-off-by: Amol Kapoor <theahura@gmail.com>

--- JS/judgr.js
+++ JS/judgr.js
@@ -6,10 +6,21 @@
 	Description: Sets up data for judging
 */
 
+
+//Request a user -> returns a user id with all hashtag data
+//For each hashtag, pull down hashtag data -> returns hashtag association data
+//Set up a list of hashtags composed of the users' data, the hashtag association data, (and some random hashtags)
+//From the list, select three hashtags at random and load them
+//On user select for a hashtag, update the userprofile score for that hashtag
+//On user select for a hashtag, clear the hashtag clicked and load another
+//On user select for a hashtag, check if hashtag update is in top five and update association accordingly
+
+
 var global_usedTags;
 var global_userTags;
 var global_userData;
 var global_nextID;
+var global_topFive;
 
 
 function requestUser() {
@@ -82,6 +93,22 @@ function requestUser() {
 					var tag = global_userTags.splice(Math.floor(Math.random()*global_userTags.length), 1)
 					$("#HashtagThree").text(tag[0]);
 					global_usedTags.push(tag[0]);
+
+					var dataObj = {};
+					
+					for(key in global_userData) {
+						if('S' in global_userData[key]) {
+							dataObj[key] = global_userData[key].S
+						}
+						else if('N' in global_userData[key])
+							dataObj[key] = parseInt(global_userData[key].N)
+					}
+
+					global_userData = dataObj;
+
+					var sortedKeys = Object.keys(dataObj).sort(function(a,b){return dataObj[a]-dataObj[b]});
+
+					global_topFive = sortedKeys.splice(sortedKeys.length, -5);
 				}
 			});
 		});
@@ -95,6 +122,40 @@ function updateProfile(hashname, value, callback) {
 		attribute: hashname,
 		value: value
 	}, function(data, err) {
+
+		global_userData[hashname] += value; 
+
+		if(global_userData[hashname] > global_userData[global_topFive[0]]) {
+
+			var kickedHashtag = global_topFive[0];
+			var newTopFive = hashname;
+
+			for(key in global_topFive) {
+				//update the two scores to reflect change
+				socket.emit('clientToServer', {
+					name: 'updateHashtagScores', 
+					hash: newTopFive,
+					attribute: key,
+					value: 1
+				}, function(data, err) {
+					console.log(data);
+					console.log(err);
+				});
+
+				socket.emit('clientToServer', {
+					name: 'updateHashtagScores', 
+					hash: kickedHashtag,
+					attribute: key,
+					value: -1
+				}, function(data, err) {
+					console.log(data);
+					console.log(err);
+				});
+			}
+			
+		}
+		
+
 		callback();
 	});
 }
